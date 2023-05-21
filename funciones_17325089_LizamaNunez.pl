4ddd9d0853a62f27898fcc7897e87c3731ef7d72
Plizama <pamela.lizama@usach.cl>
2023-05-21 17:51:30 -0400

    Capa Selectora Completa

--- funciones_17325089_LizamaNunez.pl
+++ funciones_17325089_LizamaNunez.pl
@@ -1,17 +1,42 @@
+%TDA 
+%CAPA CONSTRUCTORA
+%
 %Constructor Filesystem
 filesystem(Nombre, Usuarios, UsuarioActual, RutActual, Drives, Files, Directory, Trash,[Nombre, Usuarios, UsuarioActual, RutActual, Drives, Files, Directory, Trash]).
 
-%Make Drive
-makeDrive(LetterDrive, NameDrive, Capacity,[LetterDrive, NameDrive, Capacity]).
+%Constructor Drive
+drive(Letter, NameDrive, Capacity,[Letter, NameDrive, Capacity]).
+
+%CAPA SELECTORA.
+
+getNameSystem(SistemaActual, NombreSistema):-
+    filesystem(NombreSistema,_,_,_,_,_,_,_,SistemaActual).
+
+getListUsers(SistemaActual, ListUsers):-
+    filesystem(_,ListUsers,_,_,_,_,_,_,SistemaActual).
+
+getUserActual(SistemaActual, UserActual):-
+    filesystem(_,_,UserActual,_,_,_,_,_,SistemaActual).
+
+getRutaActual(SistemaActual, RutaActual):-
+    filesystem(_,_,_,RutaActual,_,_,_,_,SistemaActual).
+
+getDrives(SistemaActual, DrivesActuales):-
+    filesystem(_,_,_,_,DrivesActuales,_,_,_,SistemaActual).
+
+getFiles(SistemaActual, FilesActuales):-
+    filesystem(_,_,_,_,_,FilesActuales,_,_,SistemaActual).
+
+getDirectory(SistemaActual, DirectoryActuales):-
+    filesystem(_,_,_,_,_,_,DirectoryActuales,_,SistemaActual).
+
+getTrash(SistemaActual, TrashActual):-
+    filesystem(_,_,_,_,_,_,_,TrashActual,SistemaActual).
 
 %F01: TDA system - constructor.
 %creando la un nuevo sistema con nombre “NewSystem”
 %system("NewSystem", S):-
 system(Nombre, Sistema):-
-    filesystem(Nombre, [],_,_,[],[],[],[], Sistema).
+    filesystem(Nombre,[],_,[],[],[],[],[],Sistema).
    
-%F02: TDA system - addDrive.(FALTA)
-%%creando la unidad C, con nombre “OS” y capacidad “1000000000 en el sistema “NewSystem”
-%systemAddDrive(S, “C”,  “OS”, 10000000000, S2).
-systemAddDrive(OriginalSystem, LetterDrive, NameDrive, Capacity,UpdateSystem):-
     
