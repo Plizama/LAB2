68048683beb4a1e338b8d0d2c77715d224b3b6d3
Plizama <pamela.lizama@usach.cl>
2023-05-22 16:48:48 -0400

    Regla 2

--- funciones_17325089_LizamaNunez.pl
+++ funciones_17325089_LizamaNunez.pl
@@ -32,11 +32,36 @@ getDirectory(SistemaActual, DirectoryActuales):-
 
 getTrash(SistemaActual, TrashActual):-
     filesystem(_,_,_,_,_,_,_,TrashActual,SistemaActual).
+%CAPA MODIFICADORA
+
+setaddDrives(NewDrive, OriginalDrives, UpdateDrives):-
+    append(OriginalDrives, [NewDrive], UpdateDrives).
+
+setSystemNewDrives(OriginalSystem, UpdateDrives, UpdateSystem):-
+    filesystem(NameSystem, Usuarios, UsuarioActual, RutaActual,_, Files, Directory, Trash,OriginalSystem),
+    filesystem(NameSystem, Usuarios, UsuarioActual, RutaActual,UpdateDrives, Files, Directory, Trash, UpdateSystem).
+
+%OTRAS FUNCIONES
+%Existe Letter Drive?
+existeLetterDrive()
+
 
 %F01: TDA system - constructor.
 %creando la un nuevo sistema con nombre “NewSystem”
 %system("NewSystem", S):-
 system(Nombre, Sistema):-
-    filesystem(Nombre,[],_,[],[],[],[],[],Sistema).
-   
+    filesystem(Nombre,[],[],[],[],[],[],[],Sistema).
+
+
+%F02: TDA system - addDrive.(FALTA member letter).
+%%creando la unidad C, con nombre “OS” y capacidad “1000000000 en el sistema “NewSystem”
+%systemAddDrive(S, “C”,  “OS”, 10000000000, S2).
+%systemAddDrive(S, "C",  "OS", 10000000000, S2),
+systemAddDrive(OriginalSystem, LetterDrive, NameDrive, Capacity,UpdateSystem):-
+    drive(LetterDrive,NameDrive,Capacity,NewDrive),
+    getDrives(OriginalSystem, DrivesActuales),
+    \+member(LetterDrive,DrivesActuales),
+    setaddDrives(NewDrive, DrivesActuales, UpdateDrives),
+    setSystemNewDrives(OriginalSystem, UpdateDrives, UpdateSystem).
+
     
